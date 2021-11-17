ac357b0b7c13a933dace4792cdf05feb4c4abc5a
dianeegranger <59130351+dianeegranger@users.noreply.github.com>
2021-11-17 15:23:19 -0500

    Day 1 Assignment Submitted
    Install Python, install favorite text editor, and run the Hello World program.

---
+++ Day1Asgmt/Day1Asgmt.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# November 17, 2021<br/>
+# <br/>
+# Diane Granger<br/>
+# dianeegranger@gmail.com<br/>
+# <br/>
+# Day 1 Assignment<br/>
+# Python Essentials Zero to Hero<br/>
+# LetsUpgrade<br/>
+# <br/>
+# Instructor:  Kamal Seth<br/>
+
+# ### Day 1 Assignment 
+# 
+# Install Python, install favourite text editor, run hello world 
+# 
+
+# In[1]:
+
+
+print ("Hello, World!")
+
+
+# In[2]:
+
+
+print ("2+3")
+
+
+# In[3]:
+
+
+print (f"Addition of 2 + 3 is:  {2+3}")
+
+
+# In[4]:
+
+
+print (f"Mul of 2 & 3 is:  {2*3}")
+
+
+# In[5]:
+
+
+print (f"Sum of 2 and 3:  {2+3} \nMul of 2 and 3:  {2*3}")
+
+
+# In[6]:
+
+
+name = "Lets Upgrade" # comment assigning of variable
+print (f"Name: ", name)
+
+
+# In[7]:
+
+
+age = 10
+print (f"Age: ", age)
+
